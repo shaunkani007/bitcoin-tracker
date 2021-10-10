dcce589e2f51c381223917342e6254ae54b0a831
shaunkani007 <prithvish.shaunkani@gmail.com>
2021-10-10 16:55:51 +0530

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Prithvish Shaunkani on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
